5d693d841b9045f7f0e6c91c96b098f3ada72e41
dudebipin <kumarbipinp19@gmail.com>
2022-06-14 13:49:56 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// <![CDATA[
+$(function() {
+
+  // Slider
+  $('#coin-slider').coinslider({width:960,height:360,opacity:1});
+
+
+
+});	
+
+// Cufon
+Cufon.replace('.menu_nav ul li a, p.infopost a.com span, h1, h2, h3, h4, h5, h6', { hover: true });
